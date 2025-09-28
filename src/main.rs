711156e809a3c282f1556deff2ee35b812878754
dingy6269 <gearonix.twink@gmail.com>
2025-09-28 23:14:18 +0300

    merge-two-lists

--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,39 @@
-fn main() {}
-
-#[cfg(test)]
-mod tests {
-    use std::collections::HashMap;
-
-    #[test]
-    fn r1() {
-        let str = "MCMXCIV";
-        let mut result = 0;
-
-        let roman_map = HashMap::from([
-            ('I', 1),
-            ('V', 5),
-            ('X', 10),
-            ('L', 50),
-            ('C', 100),
-            ('D', 500),
-            ('M', 1000),
-        ]);
-
-        let subt_map = HashMap::from([
-            ("IV", 4),
-            ("IX", 9),
-            ("XL", 40),
-            ("XC", 90),
-            ("CD", 400),
-            ("CM", 900),
-        ]);
-
-        let mut stack = Vec::new();
-
-        for char in str.chars() {
-            let sb = stack.pop();
-
-            if matches!(char, 'I' | 'X' | 'C') {
-                stack.push(char);
-            }
-
-            if let Some(last) = sb {
-                let key = [last, char].iter().collect::<String>();
-
-                if let Some(sb_value) = subt_map.get(&key.as_str()) {
-                    result -= roman_map.get(&last).unwrap();
-                    result += sb_value;
-                    continue;
-                } 
-            }
-
-            if let Some(value) = roman_map.get(&char) {
-                result += value
-            }
-
-        }
-
-        println!("{:?}", result);
-
-        assert_eq!(result, 1994);
-    }
-
-    #[test]
-    fn r2() {
-        let numbers: Vec<i32> = vec![1, 2, 3, 4, 5];
-
-        let even_numbers = numbers
-            .into_iter()
-            .filter(|n| n % 2 == 0)
-            .collect::<Vec<i32>>();
-
-        println!("{:?}", even_numbers);
-    }
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
-
-
-use std::collections::HashMap;
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
 
 struct Solution {}
 
 impl Solution {
-    fn r1() {
-        let str = "MCMXCIV";
-        let mut result = 0;
-
-        let roman_map = HashMap::from([
-            ('I', 1),
-            ('V', 5),
-            ('X', 10),
-            ('L', 50),
-            ('C', 100),
-            ('D', 500),
-            ('M', 1000),
-        ]);
-
-        let subt_map = HashMap::from([
-            ("IV", 4),
-            ("IX", 9),
-            ("XL", 40),
-            ("XC", 90),
-            ("CD", 400),
-            ("CM", 900),
-        ]);
-
-        let mut stack = Vec::new();
-
-        for char in str.chars() {
-            let sb = stack.pop();
-
-            if matches!(char, 'I' | 'X' | 'C') {
-                stack.push(char);
+    pub fn merge_two_lists(
+        list1: Option<Box<ListNode>>,
+        list2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        match (list1, list2) {
+            (None, None) => None,
+            (Some(n), None) | (None, Some(n)) => Some(n),
+            (Some(n1), Some(n2)) => {
+                if (n1.val < n2.val) {
+                    Some(Box::new(ListNode {
+                        val: n1.val,
+                        next: Self::merge_two_lists(n1.next, Some(n2)),
+                    }))
+                } else {
+                    Some(Box::new(ListNode {
+                        val: n2.val,
+                        next: Self::merge_two_lists(Some(n1), n2.next),
+                    }))
+                }
             }
-
-            if let Some(last) = sb {
-                let key = [last, char].iter().collect::<String>();
-
-                if let Some(sb_value) = subt_map.get(&key.as_str()) {
-                    result -= roman_map.get(&last).unwrap();
-                    result += sb_value;
-                    continue;
-                } 
-            }
-
-            if let Some(value) = roman_map.get(&char) {
-                result += value
-            }
-
         }
-
-        assert_eq!(result, 1994);
     }
 }

48a34988c78c6149f09885d4a8d2b7fb41dfe22d
dingy6269 <gearonix.twink@gmail.com>
2025-09-29 15:44:42 +0300

    tranpping rain water

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,9 @@
 struct Solution {}
 
-impl Solution {
-  pub fn trap(height: Vec<i32>) -> i32 {
-    let mut outcome: i32 = 0;
 
-    let mut l = 0;
-    let mut r = height.len() - 1;
-
-    let mut l_max = height[l];
-    let mut r_max = height[r];
-
-    while l < r {
-      let left = height[l];
-      let right = height[r];
-
-      if (l_max <= r_max) {
-        l += 1;
-
-        l_max = l_max.max(left);
-
-        if left < l_max {
-          let delta = l_max - left;
-
-          outcome += delta;
-        }
-      } else {
-        r -= 1;
-        r_max = r_max.max(right);
-
-        if right < r_max {
-          let delta = r_max - right;
-
-          outcome += delta;
-        }
-      }
-    }
-
-    outcome
-  }
-}
 
 fn main() {
-  let height: Vec<i32> =
-    vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+  let height: Vec<i32> = vec![5, 5, 1, 7, 1, 1, 5, 2, 7, 6];
 
   println!("{}", Solution::trap(height));
 }

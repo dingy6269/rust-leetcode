c6e2290f4e349409d249320d88ab4052f7a9ca3b
dingy6269 <gearonix.twink@gmail.com>
2025-10-06 18:45:57 +0300

    init 4a

--- src/main.rs
+++ src/main.rs
@@ -2,180 +2,40 @@ use std::collections::HashMap;
 
 struct Solution {}
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-struct Coords {
-  x: i32, // 1..=8
-  y: i32, // 1..=8
+impl Solution {
+  pub fn codeforces_4a(w: i32) {
+    x
+
+  };
 }
 
-#[derive(Clone, Eq, PartialEq, Debug)]
-struct SquareMutator {
-  x: HashMap<char, i32>,
-  y: HashMap<char, i32>,
-}
+// single watermln
+// two instances
 
-impl SquareMutator {
-  pub fn new() -> Self {
-    let mut x_map = HashMap::new();
-    let mut y_map = HashMap::new();
+// biggest (max)
+// computed => "w"
 
-    const x_transmute: &'static str = "abcdefgh";
-    const y_transmute: &'static str = "87654321";
+// tried to divide (between 2)
+// constrains:
+// 1. % 2 == 0 (can be diff size) (yeah and >0 each)
 
-    for (i, c) in x_transmute.chars().enumerate() {
-      x_map.insert(c, (i + 1) as i32);
-    }
+// => bool
 
-    for (i, c) in y_transmute.chars().enumerate() {
-      y_map.insert(c, (i + 1) as i32);
-    }
-
-    Self { x: x_map, y: y_map }
-  }
-
-  pub fn mutate(&self, s_: impl AsRef<str>) -> (i32, i32) {
-    let s = s_.as_ref();
-    let chars: Vec<char> = s.chars().collect();
-
-    let x = self.x.get(&chars[0]).unwrap();
-    let y = self.y.get(&chars[1]).unwrap();
-
-    return (x.clone(), y.clone());
-  }
-}
-
-impl Coords {
-  pub fn new(x: i32, y: i32) -> Self {
-    Self { x, y }
-  }
-
-  pub fn update(&mut self, direction: RawDirection) {
-    self.x = self.x + direction[0];
-    self.y = self.y + direction[1];
-  }
-}
-
-// type Directions = HashMap<String, [i32; 2]>;
-type RawDirection = [i32; 2];
-
-struct Directions {
-  map: HashMap<&'static str, RawDirection>,
-}
-
-impl Directions {
-  pub fn new() -> Self {
-    let mut map = HashMap::new();
-
-    map.insert("L", [-1, 0]);
-    map.insert("R", [1, 0]);
-    map.insert("U", [0, 1]);
-    map.insert("D", [0, -1]);
-    map.insert("LU", [-1, 1]);
-    map.insert("LD", [-1, -1]);
-    map.insert("RU", [1, 1]);
-    map.insert("RD", [1, -1]);
-
-    Self { map }
-  }
-
-  pub fn transmute(
-    &self,
-    direction: impl AsRef<str>,
-  ) -> RawDirection {
-    let dir = direction.as_ref();
-
-    *self.map.get(dir).unwrap_or_else(|| {
-      panic!("Invalid direction: {}", dir);
-    })
-  }
-}
-
-// king, alone =>
-// => moves to t
-// from s to t (with min amount of moves)
+// so it can be 4 and 4
+// 2 and 6 
+// 6 and 2
 //
 
-// a8
-// h1
-
-// PREVIOSUYLLY!!!
-// a ... h === y
-// 1 ..8 == x
-
-// NEW
-
-// 1..8 == x
-// 1..8 == y
-
-// 1-1
-// 8-8
-
-// [1, 1]
-// [8, 8]
-
-impl Solution {
-  pub fn king_moves(s: Coords, t: Coords) -> () {
-    // so we find deltas
-    let mut dx = t.x - s.x;
-    let mut dy = t.y - s.y;
-    let mut moves = Vec::new();
-
-    // dx, dy
-    while dx != 0 || dy != 0 {
-      let mut mv = String::with_capacity(2);
-
-      if dx > 0 {
-        mv.push('R');
-        dx -= 1;
-      } else if dx < 0 {
-        mv.push('L');
-        dx += 1;
-      }
-
-      // -8 => -7
-      if dy > 0 {
-        mv.push('D');
-        dy -= 1;
-      } else if dy < 0 {
-        mv.push('U');
-        dy += 1;
-      }
-
-      moves.push(mv);
-    }
-
-    println!("{}", moves.len());
-
-    for mv in moves {
-      println!("{}", mv);
-    }
-
-    // so int that case
-    // instead of comparing intiail and end
-    // we can create "delta"
-    // and allocate the futher stuff
-    // to the delta
-
-    // while initial.x != end.x && initial.y != end.y {}
-  }
-}
-
-// dx = tx - sx
-
-// 8..1 = y
-// a..h = x
-
-// a8
-// h1
+// (9) the initial value sohuld be % 2 == 0
+// (10)
+/// 2 8
+/// 4 6 
+/// 6 4
+/// 8 2
+/// 
+/// 2
+/// 1 1
 
 fn main() {
-  let mutator = SquareMutator::new();
 
-  let start_m = mutator.mutate("a8");
-  let finish_m = mutator.mutate("h1");
-
-  let start = Coords::new(start_m.0, start_m.1);
-  let finish = Coords::new(finish_m.0, finish_m.1);
-
-  let result = Solution::king_moves(start, finish);
 }

0021c5dff36ced4171791687a73e8d425b35e2a9
dingy6269 <gearonix.twink@gmail.com>
2025-10-06 19:14:04 +0300

    4a stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,51 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, panic::resume_unwind};
 
 struct Solution {}
 
-impl Solution {
-  pub fn codeforces_4a(w: i32) {
-    x
-
-  };
+pub fn is_odd(x: i32) -> bool {
+  x % 2 == 0
 }
+
+impl Solution {
+  pub fn codeforces_4a(w: i32) -> bool {
+    if (!is_odd(w)) {
+      return false;
+    };
+
+    // 1
+    let mut left = 1;
+    // 7
+    let mut right = w - 1;
+
+    let mut results: Vec<(i32, i32)> = Vec::new();
+
+    // 4 4
+    while (left <= right) {
+      if (!is_odd(left)) {
+        left += 1
+      };
+
+      if (!is_odd(right)) {
+        right -= 1
+      };
+
+      // 2 6 => 3 5
+      if (is_odd(left) && is_odd(right)) {
+        results.push((left, right));
+
+        left += 1;
+        right -=1;
+      }
+    }
+
+    println!("{:?}", results);
+
+    results.len() > 0
+  }
+}
+
+// two pointers => two times
+// without colliding
 
 // single watermln
 // two instances
@@ -22,20 +60,31 @@ impl Solution {
 // => bool
 
 // so it can be 4 and 4
-// 2 and 6 
+// 2 and 6
 // 6 and 2
 //
 
 // (9) the initial value sohuld be % 2 == 0
 // (10)
 /// 2 8
-/// 4 6 
+/// 4 6
 /// 6 4
 /// 8 2
-/// 
+///
 /// 2
 /// 1 1
 
 fn main() {
 
+    let _ = Solution::codeforces_4a(8);
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::Solution;
+
+  #[test]
+  pub fn case_1() {
+    let result = Solution::codeforces_4a(8);
+  }
 }

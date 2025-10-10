794953a22cfa779b9f3f6811b3fe2cf61b8edce6
dingy6269 <gearonix.twink@gmail.com>
2025-10-10 13:43:29 +0300

    qwe

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,35 @@
-
 #[derive(Debug, PartialEq, Eq)]
 struct MinStack {
   vals: Vec<i32>,
-  mins: Vec<i32>
+  mins: Vec<i32>,
 }
 
 impl MinStack {
   fn new() -> Self {
-    Self {
-      vals: vec![],
-      mins: vec![]
-    }
+    Self { vals: vec![], mins: vec![] }
   }
 
   fn push(&mut self, val: i32) {
+    let m = self.mins.last().copied().map(|t| t.min(val)).unwrap_or(val);
+
     self.vals.push(val);
-    self.mins.
+    self.mins.push(m);
   }
 
   fn pop(&mut self) {
+    // because it has the SAME index there
+    self.mins.pop();
     self.vals.pop();
   }
 
   fn top(&self) -> i32 {
     *self.vals.last().expect("no value error")
-    }
+  }
 
   fn get_min(&self) -> i32 {
-    let value = self.vals.iter().min();
-
-    *value.expect("no value error")
+    *self.mins.last().unwrap()
   }
 }
-
 
 fn main() {
   let mut stack = MinStack::new();
@@ -46,7 +43,7 @@ fn main() {
   println!("{:?}", min);
 
   stack.pop();
-  
+
   println!("{:?}", stack.top());
   println!("{:?}", stack.get_min())
 }

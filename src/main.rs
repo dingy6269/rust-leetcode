e124d9850e6dda8a812f9bf35d0d92319ad85276
dingy6269 <gearonix.twink@gmail.com>
2025-10-18 15:49:40 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,69 @@
 use std::collections::HashSet;
 use std::io::{self, BufRead};
 
-const NODES_LEN: usize = 8;
+// sorted matrix
+// so binary search
+// lets go
+
+// it could be matrix any size
+// this is just an example
+
+// so yeah binary search
+// absolutely
+// this thiing is sorted +
+// so in 2d matrixes this is it
+
+struct Solution;
+
+impl Solution {
+  pub fn search_matrix(
+    matrix: Vec<Vec<i32>>,
+    target: i32,
+  ) -> bool {
+    let mut left = 0;
+    let mut right = matrix.len() - 1;
+
+    let mut mid_answer = -1;
+
+    // O(log n)
+    while left <= right {
+      // 0 + 2  //// 2 => 1
+      let mid = (left + right) / 2;
+
+      let first = matrix[mid][0];
+      // refactor
+      let last = matrix[mid].last().unwrap().clone();
+
+      if last < target {
+        left = mid + 1;
+      }
+
+      if first > target {
+        right = mid  - 1;
+      }
+
+      if target > first && target < last {
+        mid_answer = mid as i32;
+        break;
+      }
+    };
+
+    dbg!(mid_answer);
+
+
+    false
+  }
+}
+
+fn main() {
+  let matrix: Vec<Vec<i32>> = vec![
+    vec![1, 3, 5, 7],
+    vec![10, 11, 16, 20],
+    vec![23, 30, 34, 60],
+  ];
+
+  Solution::search_matrix(matrix, 3);
+}
 
 // 8x8
 // vertical or horizontal
@@ -25,120 +87,64 @@ const NODES_LEN: usize = 8;
 //     0
 // }
 
-fn solution(mut nodes: Vec<String>) -> i32 {
-  let mut y_vec = vec![None; NODES_LEN];
-  let mut x_vec = vec![None; NODES_LEN];
+// fn solution(y: i32, w: i32) -> f64 {
+//   ((6 - y.max(w)) + 1) as f64 / 6.0
+// }
 
-  let mut x_count = NODES_LEN;
-  let mut y_count = NODES_LEN;
+// // nothing stops you from now
+// // reload your mind
 
-  // if y_vec i is None
-  //
+// fn main() {
+//   let (y, w) = input_tuple_i32();
 
-  // nodes
-  for (i, node) in nodes.iter().enumerate() {
-    let chars: Vec<char> =
-      node.chars().take(NODES_LEN).collect();
+//   let sol = solution(y, w);
 
-    // W - [1]
-    // x <= [1]
-    // y <= [4]
+//   send(sol);
+// }
 
-    // chars
-    for j in 0..NODES_LEN {
-      let unit = chars[j];
+// fn send<T>(data: T)
+// where
+//   T: std::fmt::Display,
+// {
+//   println!("{}", data);
+// }
 
-      // - matters here
-      if y_vec[i] != Some('-') {
-        if y_vec[i].is_none() {
-          y_vec[i] = Some(unit);
-        } else {
-          if unit != y_vec[i].unwrap() {
-            y_count = y_count.saturating_sub(1);
-            y_vec[i] = Some('-');
-          }
-        }
-      }
+// fn input() -> String {
+//   let mut input = String::new();
 
-      if x_vec[j] != Some('-') {
-        if x_vec[j].is_none() {
-          x_vec[j] = Some(unit);
-        } else {
-          if chars[j] != x_vec[j].unwrap() {
-            x_count = x_count.saturating_sub(1);
-            x_vec[j] = Some('-');
-            // ??
-          }
-        }
-      }
-    }
-  }
+//   io::stdin()
+//     .read_line(&mut input)
+//     .expect("Failed to read line");
 
-  //   dbg!(x_count);
-  //   dbg!(y_count);
+//   input.trim().to_string()
+// }
 
-  //   dbg!(x_vec);
-  //   dbg!(y_vec);
+// fn input_i32() -> i32 {
+//   let input = input();
+//   let mut n: i32 = input.trim().parse().unwrap();
 
-  (x_count + y_count) as i32
-}
+//   n
+// }
 
-fn main() {
-  let mut nodes = vec![];
+// fn input_tuple_i32() -> (i32, i32) {
+//   let input = input();
 
-  for _ in 0..8 {
-    let i = input();
-    nodes.push(i);
-  }
+//   let (n, k): (i32, i32) = {
+//     let (a, b) = input.trim().split_once(" ").unwrap();
 
-  let sol = solution(nodes);
+//     (a.trim().parse().unwrap(), b.trim().parse().unwrap())
+//   };
 
-  send(sol);
-}
+//   return (n, k);
+// }
 
-fn send<T>(data: T)
-where
-  T: std::fmt::Display,
-{
-  println!("{}", data);
-}
+// fn input_vec_i32() -> Vec<i32> {
+//   let input = input();
 
-fn input() -> String {
-  let mut input = String::new();
+//   let nums: Vec<i32> = input
+//     .split_whitespace()
+//     .map(|s| s.parse().expect("Not an integer!"))
+//     .collect();
 
-  io::stdin()
-    .read_line(&mut input)
-    .expect("Failed to read line");
-
-  input.trim().to_string()
-}
-
-fn input_i32() -> i32 {
-  let input = input();
-  let mut n: i32 = input.trim().parse().unwrap();
-
-  n
-}
-
-fn input_tuple_i32() -> (i32, i32) {
-  let input = input();
-
-  let (n, k): (i32, i32) = {
-    let (a, b) = input.trim().split_once(" ").unwrap();
-
-    (a.trim().parse().unwrap(), b.trim().parse().unwrap())
-  };
-
-  return (n, k);
-}
-
-fn input_vec_i32() -> Vec<i32> {
-  let input = input();
-
-  let nums: Vec<i32> = input
-    .split_whitespace()
-    .map(|s| s.parse().expect("Not an integer!"))
-    .collect();
-
-  nums
-}
+//   nums
+// }

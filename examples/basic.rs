4dfe93d46ba7e6bec4129863bf86dc881f28055c
dingy6269 <gearonix.twink@gmail.com>
2025-10-18 21:46:25 +0300

    update

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,8 +1,13 @@
 use std::rc::Rc;
 
 pub fn main() {
-  let a = Rc::new(5);
-  let b = a.clone();
+  // let a = Rc::new(5);
+  // let b = a.clone();
 
-  println!("{}, {}", a, b);
+  // println!("{}, {}", a, b);
+
+  // 3
+  let t = (3.0 as f64 / 4.0 as f64).ceil();
+
+  println!("{:?}", t);
 }
--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,100 @@ use std::io::{self, BufRead};
 
 // daily temperatures
 // answer[i]
-// 
+//
 
 struct Solution;
 
-
 impl Solution {
-    pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
-        // 2
-        let corr = h as f64 / (piles.len() as f64);
+  pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
+    // 2
+    let corr = h as f64 / (piles.len() as f64);
 
-        let min = i32::MAX;
-        let max = i32::MIN;
+    let mut min = i32::MAX;
+    let mut max = i32::MIN;
 
-        for (i, pile) in piles.into_iter().enumerate() {
-            let cel = pile as f64 / corr;
+    for (i, pile) in piles.iter().enumerate() {
+      let cel = (*pile) as f64 / corr;
 
-            min = min.min(cel.floor() as i32);
-            max = max.max(cel.ceil() as i32);
-        };
+      min = min.min(cel.floor() as i32);
+      max = max.max(cel.ceil() as i32);
     }
+
+    // acting like an array with indexes
+    let mut left = min.clone();
+    let mut right = max.clone();
+
+    // EXAMPLE
+    // ((3))
+
+    // [3]
+    // 3 % 3 == 0
+    // 6 % 3 == 0
+
+    let mut bmin: i32 = i32::MAX;
+
+    while left <= right {
+      let mid = (left + right) / 2;
+
+      let mut bsum = 0;
+
+      for (j, node) in piles.iter().enumerate() {
+        // 3 => 10
+        // 1
+        // 2
+        // 3
+        // 4
+        let div = (*node as f64 / mid as f64);
+        let amount = div.ceil() as i32;
+
+        bsum += amount;
+
+
+        // [4] 8 == 8
+      }
+      dbg!(mid, bsum, h);
+
+      if bsum == h {
+        bmin = mid;
+        break;
+      }
+
+      // мид должен стать больше
+      // здесь reverse
+      if bsum > h {
+        left = mid + 1;
+      }
+
+      if bsum < h {
+        right = mid - 1;
+      }
+    }
+
+    // continue find the final min
+    // this is last block
+
+    dbg!(bmin);
+
+    -1
+  }
 }
 
 fn main() {
+  Solution::min_eating_speed(vec![3, 6, 7, 11], 8);
+
+  println!("------");
+
+  Solution::min_eating_speed(vec![30, 11, 23, 4, 20], 5);
+
+  println!("------");
+
+  Solution::min_eating_speed(vec![30, 11, 23, 4, 20], 6);
 
 }
 
 // [3, 6, 7, 11]
 
-// 3 
+// 3
 
 // 4
 // 2
@@ -44,11 +109,9 @@ fn main() {
 // 4
 // 3
 
-
 // corr h / len
 
 // 8 / 4 = 2
-
 
 // 1.5
 // 3
@@ -64,12 +127,12 @@ fn main() {
 // 30
 // 11
 // 23
-// 4 
-// 20 
+// 4
+// 20
 
 // =============
 
-// corr h / len 
+// corr h / len
 
 // 5 / 5 == 1
 
@@ -81,4 +144,4 @@ fn main() {
 // corr h / len
 
 // 6 / 5 == 1.25
-// 
+//
